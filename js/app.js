3b9516131b59a2a4404af8d4a325a97a208150fe
Yologram <metais.louis@hotmail.fr>
2015-12-21 17:34:46 +0100

    modif senna

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 "use strict";
 
+var apl = new senna.App();
+
+	apl.setBasePath('/');
+	apl.addSurfaces('sennastuff');
+	apl.addRoutes(new senna.Route(/\w+\.html/,senna.HtmlScreen));
+
 var app = {
 	config: {
 		url:"https://smp.ovh",
